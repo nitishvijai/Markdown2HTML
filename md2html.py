3d9669123bd87317ee75f349821f080e42989611
nitishv <nitishv@umich.edu>
2020-06-16 00:20:43 -0400

    Added imports and initialized script

---
+++ md2html.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# @author: Nitish Vijai
+# <nitishv@umich.edu>
+# https://www.github.com/nitishvijai/Markdown2HTML
+# md2html.py
+
+import argparse
+import os
+import sys
+
+if __name__ == "__main__":
+    print("Hello, World")
