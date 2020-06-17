f4418b35f641745a22d5598e7a02ffd3fe841ab6
nitishv <nitishv@umich.edu>
2020-06-17 13:28:40 -0400

    Added conversion support for paragraphs and line breaks

--- md2html.py
+++ md2html.py
@@ -25,38 +25,85 @@ Options:
 from docopt import docopt
 import os
 from os import path
+import re
 import sys
 
 verbose = True
 
 def convert(source, dest, args, verbosity):
     dest = open(args['--dest'], 'w')
+    prevLine = ''
+    nested = ''
     for line in source:
         newLine = ''
+
+        # ------------------------- Headings (h1 - h6) -------------------------
         if line.startswith('# '): # heading level 1
             content = line[2:len(line) - 1]
+            nested = 'h1'
             newLine = '<h1>' + content + '</h1>\n'
             dest.write(newLine)
         elif line.startswith('## '): # heading level 2
             content = line[3:len(line) - 1]
+            nested = 'h2'
             newLine = '<h2>' + content + '</h2>\n'
             dest.write(newLine)
         elif line.startswith('### '): # heading level 3
             content = line[4:len(line) - 1]
+            nested = 'h3'
             newLine = '<h3>' + content + '</h3>\n'
             dest.write(newLine)
         elif line.startswith('#### '): # heading level 4
             content = line[5:len(line) - 1]
+            nested = 'h4'
             newLine = '<h4>' + content + '</h4>\n'
             dest.write(newLine) 
         elif line.startswith('##### '): # heading level 5
             content = line[6:len(line) - 1]
+            nested = 'h5'
             newLine = '<h5>' + content + '</h5>\n'
             dest.write(newLine)   
         elif line.startswith('###### '): # heading level 6
             content = line[7:len(line) - 1]
+            nested = 'h6'
             newLine = '<h6>' + content + '</h6>\n'
-            dest.write(newLine)      
+            dest.write(newLine)   
+        elif line.startswith('=='): # alt. syntax for level 1
+            content = prevLine
+            nested = 'h1'
+            newLine = '<h1>' + content + '</h1>\n'
+            lines = dest.readlines()
+            lines = lines[:-1]
+            dest.write(newLine)
+        # implement alt. syntax for level 2 later
+
+        # ------------------------- Paragraphs (p) -------------------------
+        if re.match('^[a-zA-Z]+', line) is not None:
+            newLine = '<p>' + line + '</p>\n'
+            nested = 'p'
+            dest.write(newLine)
+        
+        # ------------------------- Line Breaks (br) -------------------------
+        if line.endswith('  '):
+            if nested is not None:
+                line.replace('  ', '<br>')
+                newLine = '<' + nested + '>' + line + '</' + nested + '>\n'
+                lines = dest.readlines()
+                lines = lines[:-1]
+                dest.write(newLine)
+            else:
+                line.replace('  ', '<br>')
+                dest.write(line)
+        elif line.endswith('<br>'):
+            if nested is not None:
+                newLine = '<' + nested + '>' + line + '</' + nested + '>\n'
+                lines = dest.readlines()
+                lines = lines[:-1]
+                dest.write(newLine)
+            else:
+                dest.write(line)
+            
+
 
         if line == '': # blank line
             dest.write('\n\n')  
@@ -64,6 +111,8 @@ def convert(source, dest, args, verbosity):
         if verbosity:
             print("Source: " + line)
             print("Destination: " + newLine)
+
+        prevLine = line
 
 
 def create_files(args, verbosity):
